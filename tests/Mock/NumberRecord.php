79355bac88ba1bfe814284d2accce00a5c97c8fa
Roman Varkuta <roman.varkuta@gmail.com>
2019-02-20 17:20:18 +0200

    Add parameter to specifying types of attributes (#3)

---
+++ tests/Mock/NumberRecord.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Kartavik\Yii2\Tests\Mock;
+
+use Kartavik\Yii2;
+use yii\db;
+
+/**
+ * Class NumberRecord
+ * @package Kartavik\Yii2\Tests\Mock
+ *
+ * @property int $id
+ * @property TestEnum $first
+ * @property TestEnum $second
+ */
+class NumberRecord extends db\ActiveRecord
+{
+    public static function tableName()
+    {
+        return 'record';
+    }
+
+    public function behaviors(): array
+    {
+        return [
+            'enum' => [
+                'class' => Yii2\Behaviors\EnumMappingBehavior::class,
+                'enumsAttributes' => [
+                    NumericEnum::class => [
+                        'first',
+                        'second',
+                    ],
+                ],
+                'attributesType' => [
+                    'first' => 'integer',
+                    'second' => 'float'
+                ]
+            ],
+        ];
+    }
+
+    public function rules()
+    {
+        return [
+            [
+                ['first', 'second'],
+                Yii2\Validators\EnumValidator::class,
+                'targetEnum' => NumericEnum::class,
+            ]
+        ];
+    }
+}

751edacdfe7cd74b3f7cc7f6e69df0d2494f76ee
Kartvaik <roman.varkuta@gmail.com>
2019-02-27 12:39:37 +0200

    Fix mysql migration extending

--- src/Database/Mysql/Migration.php
+++ src/Database/Mysql/Migration.php
@@ -2,11 +2,13 @@
 
 namespace Kartavik\Yii2\Database\Mysql;
 
+use yii\db;
+
 /**
  * Class Migration
  * @package Kartavik\Yii2\Database\Mysql
  */
-class Migration
+class Migration extends db\Migration
 {
     use MigrationTrait;
 }
--- tests/Migration/M190226185952TestMysqlMigration.php
+++ tests/Migration/M190226185952TestMysqlMigration.php
@@ -15,10 +15,12 @@ class M190226185952TestMysqlMigration extends Mysql\Migration
      */
     public function safeUp()
     {
-        $this->createTable('test_table', [
-            'enum_column_from_array' => $this->enum(['1', '2', '3', '4']),
-            'enum_column_from_enum' => $this->enum(TestEnum::class)
-        ]);
+        if ($this->getDb()->getDriverName() === 'mysql') {
+            $this->createTable('test_table', [
+                'enum_column_from_array' => $this->enum(['1', '2', '3', '4']),
+                'enum_column_from_enum' => $this->enum(TestEnum::class)
+            ]);
+        }
     }
 
     /**
@@ -26,6 +28,8 @@ class M190226185952TestMysqlMigration extends Mysql\Migration
      */
     public function safeDown()
     {
-        $this->dropTable('test_table');
+        if ($this->getDb()->getDriverName() === 'mysql') {
+            $this->dropTable('test_table');
+        }
     }
 }

031f852a37e08203b44a3b7b35403ec7034f0275
user-rq <roman.varkuta@gmail.com>
2019-08-08 13:14:25 +0300

    Fix calling unknown method

--- src/Database/Pgsql/MigrationTrait.php
+++ src/Database/Pgsql/MigrationTrait.php
@@ -27,7 +27,7 @@ trait MigrationTrait
      */
     public function addEnum(string $name, $enums): void
     {
-        $enums = $this->convertEnums($enums);
+        $enums = $this->fetchEnums($enums);
 
         if (!\is_array($enums)) {
             throw new db\Exception('Variable [values] must be an array or be instance of ' . Enum::class);
